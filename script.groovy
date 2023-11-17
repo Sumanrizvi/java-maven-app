722cb0579ed5262cebb48987576610d8a84b9c0c
Suman Rizvi <sumanrizvi@gmail.com>
2023-11-17 13:49:13 -0800

    changing groovy file

--- script.groovy
+++ script.groovy
@@ -5,6 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
+    sh 'ls -l /target'
     withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t sumanrizvi/ec2-jenkins-pipeline:1.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
